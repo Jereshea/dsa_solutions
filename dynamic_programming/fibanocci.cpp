a6bd5412a5fb8cd60ab63ebfec9f5bc1ec3f29c3
Jereshea JM <jeresheajohn@gmail.com>
2025-12-21 04:05:23 +0000

    Add solution to find the Fibonacci Series using Dynamic Programming

---
+++ dynamic_programming/fibanocci.cpp
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Dynamic Programming
+Fibonacci Series
+
+*******************************************************************************/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int fibonacci_series(vector<int> &dp_array, int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+
+  if (dp_array[n] != -1) {
+    return dp_array[n];
+  }
+  // Step:2 Memoization
+  dp_array[n] =
+      fibonacci_series(dp_array, n - 1) + fibonacci_series(dp_array, n - 2);
+
+  return dp_array[n];
+}
+int main() {
+  cout << "Fibonacci Series" << endl;
+  int n = 6;
+
+  // Step:1 Create a dp_array/hash map
+  vector<int> dp_array(n + 1, -1);
+  cout << fibonacci_series(dp_array, n);
+
+  return 0;
+}
